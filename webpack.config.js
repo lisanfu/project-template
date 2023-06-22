203b950b91bafc86078828b86ffeb605ab58b870
lifuyuan <874359873@qq.com>
2023-06-22 10:31:31 +0800

    build(javascript): test

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,26 @@
 const path = require('path');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const ConsoleLogOnBuildWebpackPlugin = require('./lib/plugins/ConsoleLogOnBuildWebpackPlugin');
 module.exports = {
 	mode: 'development',
 	entry: {
-		main: './src/main.js',
 		index: {
-			dependOn: 'main',
 			import: './src/index.js',
+			dependOn: 'shared',
 		},
+		another: {
+			import: './src/another-module.js',
+			dependOn: 'shared',
+		},
+		shared: 'lodash',
 	},
 	output: {
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].[contenthash].js',
 		clean: true,
-		publicPath: '/',
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: './dist',
 	},
 	optimization: {
-		runtimeChunk: 'single',
+		// runtimeChunk: 'single',
+		splitChunks: {
+			chunks: 'all',
+		},
 	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpe?g|gif|webp)$/i,
-				type: 'asset/resource',
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/resource',
-			},
-			{
-				test: /\.(csv|tsv)$/i,
-				use: ['csv-loader'],
-			},
-			{
-				test: /\.xml$/i,
-				use: ['xml-loader'],
-			},
-		],
-	},
-	plugins: [
-		new webpack.ProgressPlugin(),
-		// new CleanWebpackPlugin(),
-		new ConsoleLogOnBuildWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			teemplate: './public/index.html',
-		}),
-	],
 };
