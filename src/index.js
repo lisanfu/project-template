f93618ea41dfa19508966be56f89ebed2ea5b37e
lifuyuan <874359873@qq.com>
2023-06-22 17:44:01 +0800

    build(javascript): test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 // require('./index.css');
+import(/* webpackPrefetch: true*/ './another-module.js');
 // console.log('1221');
 // import Icon from '../assets/img.jpeg';
 // import dataXml from './data.xml';
 // import dataCsv from './data.csv';
-import _ from 'lodash';
-function component() {
+// import _ from 'lodash';
+// function component() {
+// 	const element = document.createElement('div');
+// 	element.innerHTML = _.join(['hello', 'webpack --watch'], '');
+// 	element.classList.add('hello');
+// 	// const myIcon = new Image();
+// 	// myIcon.src = Icon;
+// 	// element.appendChild(myIcon);
+// 	// console.log(dataCsv);
+// 	// console.log(dataXml);
+// 	return element;
+// }
+// document.body.appendChild(component());
+
+// function getComponent() {
+// 	return import('lodash').then(({ default: _ }) => {
+// 		const element = document.createElement('div');
+// 		element.innerHTML = _.join(['hello webpack'], '');
+// 		return element;
+// 	});
+// }
+
+async function getComponent() {
 	const element = document.createElement('div');
-	element.innerHTML = _.join(['hello', 'webpack --watch'], '');
-	element.classList.add('hello');
-	// const myIcon = new Image();
-	// myIcon.src = Icon;
-	// element.appendChild(myIcon);
-	// console.log(dataCsv);
-	// console.log(dataXml);
+	const { default: _ } = await import('lodash');
+	element.innerHTML = _.join(['hello', 'webpack'], '');
 	return element;
 }
-document.body.appendChild(component());
+
+getComponent().then(component => {
+	document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	mode: 'development',
 	entry: {
-		index: {
-			import: './src/index.js',
-			dependOn: 'shared',
-		},
-		another: {
-			import: './src/another-module.js',
-			dependOn: 'shared',
-		},
-		shared: 'lodash',
+		// index: {
+		// 	import: './src/index.js',
+		// 	dependOn: 'shared',
+		// },
+		// another: {
+		// 	import: './src/another-module.js',
+		// 	dependOn: 'shared',
+		// },
+		// shared: 'lodash',
+
+		index: './src/index.js',
 	},
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
 	},
-	optimization: {
-		// runtimeChunk: 'single',
-		splitChunks: {
-			chunks: 'all',
-		},
-	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
+	// optimization: {
+	// 	// runtimeChunk: 'single',
+	// 	splitChunks: {
+	// 		chunks: 'all',
+	// 	},
+	// },
 };
